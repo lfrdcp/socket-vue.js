f7ce38b9b3c594bb47391eac7604e70d1e4ede10
lfrdcp <alfredocaspor1997@gmail.com>
2020-07-10 17:51:57 -0500

    Se agregó page para bienvenida a todos

---
+++ src/pages/PageMain.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-content>
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-img
+            :src="require(`../assets/img/posbc.png`)"
+            aspect-ratio="1.5"
+            contain
+          />
+
+          <h2 align="center" justify="center">
+            Punto de venta desarrollado por boxcode
+          </h2>
+          <br />
+          <v-divider></v-divider>
+          <br />
+
+          <v-row align="center" justify="center">
+            <v-col>
+              <v-btn block color="primary" to="/login">Iniciar sesion </v-btn>
+            </v-col>
+
+            <v-col>
+              <v-btn block color="primary" to="/register">Registrarse </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: 'PageMain',
+};
+</script>
--- src/router/modules/sign.js
+++ src/router/modules/sign.js
@@ -1,11 +1,16 @@
 import PageError404 from '../../pages/PageError404.vue';
 import PageLogin from '../../pages/PageLogin.vue';
 import PageRegister from '../../pages/PageRegister.vue';
+import PageMain from '../../pages/PageMain.vue';
 
 export default [
   {
     path: '*',
     component: PageError404,
+  },
+  {
+    path: '/',
+    component: PageMain,
   },
   {
     path: '/login',
